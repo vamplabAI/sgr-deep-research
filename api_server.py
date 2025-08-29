0fc8daa1a6811ce5c45840a559fa101771fb5b09
Dmitry Sirakov [Shade] <dmitrysirakov@gmail.com>
2025-08-30 00:37:47 +0300

    fix ports conflict: web and api

--- api_server.py
+++ api_server.py
@@ -1002,5 +1002,5 @@ async def root():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "api_server:app", host="0.0.0.0", port=8000, reload=True, log_level="info"
+        "api_server:app", host="0.0.0.0", port=8080, reload=True, log_level="info"
     )
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 [ "x$APP_WORKERS" = "x" ] && export APP_WORKERS="1"
 [ "x$APP_BIND" = "x" ] && export APP_BIND="0.0.0.0"
-[ "x$APP_PORT" = "x" ] && export APP_PORT="8000"
+[ "x$APP_PORT" = "x" ] && export APP_PORT="8080"
 
 uvicorn \
   --host ${APP_BIND} \
