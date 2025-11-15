175d553035a2c9a3e13e723f42b3e51313273e13
Lysenko Artem <lysenko.artem.g@gmail.com>
2025-11-15 13:10:03 +0100

    * CORS

--- sgr_deep_research/__main__.py
+++ sgr_deep_research/__main__.py
@@ -36,12 +36,10 @@ def main():
     config.agents.update(get_default_agents_definitions())
     config.definitions_from_yaml(args.agents_file)
     app = FastAPI(title="SGR Deep Research API", version=__version__, lifespan=lifespan)
+    # Don't use this CORS setting in production!
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "http://127.0.0.1:5173",
-            "http://localhost:5173",
-        ],
+        allow_origins=["*"],
         allow_credentials=["*"],
         allow_methods=["*"],
         allow_headers=["*"],
